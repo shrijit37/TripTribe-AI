caac07dc34664213cd939e05c9877c84cfc33566
Shrijit Srivastav <shrijit@Shrijits-MacBook-Pro.local>
2025-06-30 09:25:51 +0530

    seo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const script = document.createElement("script");
 script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_PLACES_API_KEY}&libraries=places`;
@@ -10,6 +11,7 @@ document.head.appendChild(script);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SpeedInsights />
     <App />
   </StrictMode>,
 )
